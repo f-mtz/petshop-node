dcb247962ec760dddfe4eaaf3a1a210141bf910c
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-05-19 20:05:20 -0300

    Criando GET (findAll) para consultas no banco de dados

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,8 +1,10 @@
 const roteador = require('express').Router();
+const TabelaFornecedor = require('./TabelaFornecedor')
 
 // Vai exibir o Ok direto no navegador
-roteador.get('/', (requisicao, resposta) => {
-    resposta.send('Ok')
+roteador.get('/', async (requisicao, resposta) => {
+    const resultados = await TabelaFornecedor.listar()
+    resposta.send(JSON.stringify(resultados))
 })
 
 module.exports = roteador
