c3ed029f10430da420c609b02e2a1b6518341155
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-05-19 20:51:40 -0300

    POST fornecedores

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,10 +1,21 @@
-const roteador = require('express').Router();
+const roteador = require('express').Router()
 const TabelaFornecedor = require('./TabelaFornecedor')
+const Fornecedor = require('./Fornecedor')
 
 // Vai exibir o Ok direto no navegador
 roteador.get('/', async (requisicao, resposta) => {
     const resultados = await TabelaFornecedor.listar()
     resposta.send(JSON.stringify(resultados))
+})
+
+
+roteador.post('/', async (requisicao, resposta) => {
+    const dadosRecebidos = requisicao.body
+    const fornecedor = new Fornecedor(dadosRecebidos)
+    await fornecedor.criar()
+    resposta.send(
+        JSON.stringify(fornecedor)
+        )
 })
 
 module.exports = roteador
