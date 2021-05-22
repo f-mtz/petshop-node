2177e662189280a3f0307dab51eeed48dc093d3f
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-05-21 21:19:56 -0300

    Trabalhando com XML

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,34 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
 
+    xml(dados) {
+        let tag = this.tagSingular
+
+        if(Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+
+        return jsontoxml({ [tag]: dados })
+    }
+
     serializar(dados) {
+        dados = this.filtrar(dados)
         if(this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+
+        if(this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -48,6 +67,8 @@ class SerializadorFornecedor extends Serializador {
             'empresa', 
             'categoria'
         ].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -59,6 +80,8 @@ class SerializadorErro extends Serializador {
             'id', 
             'mensagem'
         ].concat(camposExtras || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -66,5 +89,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
